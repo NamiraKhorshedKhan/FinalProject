39a00e3e8bb66fc086b4882946dae1efbfb46c0a
19-41055-2 <19-41055-2@student.aiub.edu>
2022-12-25 12:36:52 +0600

    ok

--- FinalProject/Controllers/CustomerController.cs
+++ FinalProject/Controllers/CustomerController.cs
@@ -1,11 +1,109 @@
-﻿using System;
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+ 
 namespace FinalProject.Controllers
 {
-    public class CustomerController
+    public class CustomerController : ApiController
     {
+        //[RoutePrefix("api/customer")]
+        [EnableCors("*", "*", "*")]
+        public class CustomersController : ApiController
+        {
+            [Route("api/customer/all")]
+            public HttpResponseMessage Get()
+            {
+                try
+                {
+                    var data = CustomerService.Get();
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+                }
+            }
+            [Route("api/customer/{id}")]
+            public HttpResponseMessage Get(string id)
+            {
+                try
+                {
+                    var data = CustomerService.Get(id);
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+                }
+            }
+            [Route("api/category/{id}/customers")]
+            [HttpGet]
+            public HttpResponseMessage GetCtCustomer(string id)
+            {
+                try
+                {
+                    var data = CustomerService.Get(id);
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+                }
+            }
+            [Route("api/customer/add")]
+            [HttpPost]
+            public HttpResponseMessage Add(CustomerDTO ct)
+            {
+                try
+                {
+                    var data = CustomerService.Add(ct);
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+                }
+
+            }
+
+            [Route("api/customer/update/{id}")]
+            [HttpPut]
+            public HttpResponseMessage Put(CustomerDTO ct)
+            {
+                try
+                {
+                    CustomerService.Update(ct);
+                    return Request.CreateResponse(HttpStatusCode.OK, "customer updated successfully");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error updating customer", e);
+                }
+            }
+
+            [Route("api/category/delete/{id}")]
+            [HttpDelete]
+            public HttpResponseMessage Delete(string id)
+            {
+                try
+                {
+                    CustomerService.Delete(id);
+                    return Request.CreateResponse(HttpStatusCode.Created, "customer delete successfully");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error deleting customer", e);
+                }
+            }
+        }
     }
 }
+
--- FinalProject/Controllers/RestaurantController.cs
+++ FinalProject/Controllers/RestaurantController.cs
@@ -1,11 +1,86 @@
-﻿using System;
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 
 namespace FinalProject.Controllers
 {
-    public class RestaurantController
+    
+    public class RestaurantController: ApiController
     {
+        [Route("api/Restaurants")]
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            var data = RestaurantService.Get();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Restaurants/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Get(string id)
+        {
+            var data = RestaurantService.Get(id);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+
+        [Route("api/Restaurants/add")]
+        [HttpPost]
+        public HttpResponseMessage Add(RestaurantDTO Restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = RestaurantService.Add(Restaurant);
+                if (data != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+        }
+
+        [Route("api/Restaurants/Delete/{id}")]
+        [HttpDelete]
+        public HttpResponseMessage Delete(string id)
+        {
+            try
+            {
+                RestaurantService.Delete(id);
+                return Request.CreateResponse(HttpStatusCode.Created, "Restaurant remove successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error removing Restaurant", e);
+            }
+        }
+
+        [Route("api/Restaurants/Update/{id}")]
+        [HttpPut]
+        public HttpResponseMessage Put(RestaurantDTO cat)
+        {
+            try
+            {
+                RestaurantService.Update(cat);
+                return Request.CreateResponse(HttpStatusCode.OK, "Restaurant updated successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error updating Restaurant", e);
+            }
+        }
+
     }
 }
